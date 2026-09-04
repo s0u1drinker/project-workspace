620f36101a292a64593a6f9dae5bbc15cd281cd7
s0u1drinker <norkiw40k@gmail.com>
2026-09-04 16:57:41 +0300

    chore(web/types): Добавлен тип TValuesEqualsKeys для объектов с одинаковыми ключами и значениями

--- apps/web/src/lib/types/app.ts
+++ apps/web/src/lib/types/app.ts
@@ -1,9 +1,17 @@
 import type { AriaAttributes } from 'svelte/elements';
 
+/** Тип для списка ошибок. */
 export type TErrorMap = Record<string, string>;
 
+/** Data-атрибуты. */
 export type DataAttributes = {
   [key: `data-${string}`]: string | undefined;
 };
 
+/** Тип для доступности: aria-атрибуты + data-атрибуты. */
 export type TAccessibilityProps = AriaAttributes & DataAttributes;
+
+/** Список значений, в которых "Имя свойства" === "Значение". */
+export type TValuesEqualsKeys<K extends PropertyKey> = {
+  [P in K]: P;
+};
